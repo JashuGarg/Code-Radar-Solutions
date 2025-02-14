5c7fc2cf819e0e84bfbc1e086a99f465f9b0f7d3
Jashu Garg <jashugarg101@gmail.com>
2025-02-14 11:31:22 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,13 @@
+// Your code here...
+#include <stdio.h>
+ int main (){
+    int a ,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
+    if (c="+"){
+        printf("%d",a+b);
+    }else {
+        printf("Not Eligible");
+    
+    }
+ }
