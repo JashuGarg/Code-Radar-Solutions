fb5e3d9135f64182060221f7da482052292b5833
Jashu Garg <jashugarg101@gmail.com>
2025-02-14 11:33:33 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,8 +6,12 @@
     scanf("%d %d %c",&a,&b,&c);
     if (c== '+'){
         printf("%d",a+b);
-    }else {
-        printf("Not Eligible");
-    
-    }
+    }else if (c=='-') {
+        printf("%d",a-b);
+    }else if (c=='*'){
+        printf("%d",a*b);
+    }else (
+        printf("%d",a/b);
+    )
+    return 0;
  }
