54add35f90a4342d34db2a26fd4ff4da4b880673
Jashu Garg <jashugarg101@gmail.com>
2025-03-07 20:42:15 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,14 @@
+// Your code here...M
+int fibonacciSeries(int n){
+    int a = 0;
+    int b = 1;
+    arr[0]=0;
+    arr[1]=1;
+    for (int i=2;i<n+2;i++){
+        c = a+b;
+        a = b;
+        b = c;
+        arr[i]=c;
+    }
+    return arr[];
+}
