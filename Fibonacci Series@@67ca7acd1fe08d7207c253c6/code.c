0f416b22c31d36989d920308b985b407bf88aad7
Jashu Garg <jashugarg101@gmail.com>
2025-03-07 20:44:32 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,14 +2,11 @@
 int fibonacciSeries(int n){
     int a = 0;
     int b = 1;
-    int arr[100000];
-    arr[0]=0;
-    arr[1]=1;
+    jcout<<a << " "<< b;
     for (int i=2;i<n+2;i++){
       int c = a+b;
         a = b;
         b = c;
-        arr[i]=c;
+        cout<<c<< " ";
     }
-    return arr;
 }
