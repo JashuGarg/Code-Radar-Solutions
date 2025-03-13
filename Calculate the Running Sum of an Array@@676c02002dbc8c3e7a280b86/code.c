5502b631311f8e76f8954f4941ef37e7fd9f9e86
Jashu Garg <jashugarg101@gmail.com>
2025-03-13 10:05:05 +0530

    Solution to Calculate the Running Sum of an Array

--- Calculate the Running Sum of an Array@@676c02002dbc8c3e7a280b86/code.c
+++ Calculate the Running Sum of an Array@@676c02002dbc8c3e7a280b86/code.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main (){
-   int arr[1000],n;
-
+   int n;
     scanf("%d",&n);
+    int arr[n];
     for (int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int ans[1000];
+    int ans[n];
 
     ans[0]=arr[0];
     for(int i=1;i<n;i++)
