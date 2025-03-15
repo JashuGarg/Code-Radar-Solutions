243983d1bc2bbadbb7f5f20be70a6617b856c0cf
Jashu Garg <jashugarg101@gmail.com>
2025-03-15 21:22:37 +0530

    Solution to Calculate Employee Bonus

--- Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -9,11 +9,15 @@ int main (){
     for(int i=0;i<n;i++){
         scanf("%d %s %f",&rollno[i],name[i],&sal[i]);
     }
-    for(int i =0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        if (sal[i]<50000)
-            printf("Employee ID: %d, Name: %s, Bonus: %.2f\n",rollno[i],name[i],(10/100)*sal[i]);
-        else 
-            printf("Employee ID: %d, Name: %s, Bonus: %.2f\n",rollno[i],name[i],(5/100)*sal[i]);
+        if (sal[i]<50000){
+        float bonus = (10/100)*sal[i]
+            printf("Employee ID: %d, Name: %s, Bonus: %.2f\n",rollno[i],name[i],bonus);}
+        else {
+            float bonus = (5/100)*sal[i]
+            printf("Employee ID: %d, Name: %s, Bonus: %.2f\n",rollno[i],name[i],bonus);
     }
+    }
+
 }
