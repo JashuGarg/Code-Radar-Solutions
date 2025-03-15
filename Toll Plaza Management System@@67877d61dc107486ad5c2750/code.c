a7d8aaa6041ef96660d53c4a1c100a0271e708b3
Jashu Garg <jashugarg101@gmail.com>
2025-03-15 21:39:09 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-int main (){
+#include <string.h>
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     char Number[n][1000];
     char Vehi[n][100];
-    int price[100];
-    for(int i=0;i<n;i++){
-        scanf("%s %s %d",Number[i],Vehi[i],&price[i]);
+    int price[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s %s %d", Number[i], Vehi[i], &price[i]);
     }
 
-    float car =0;
-    float truck =0;
-    float bike =0;
-    for (int i=0;i<n;i++){
-        if (Vehi[i]=="Car")
-            car+= price[i];
-        else if (Vehi[i]=="Truck")
+    float car = 0;
+    float truck = 0;
+    float bike = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (strcmp(Vehi[i], "Car") == 0)
+            car += price[i];
+        else if (strcmp(Vehi[i], "Truck") == 0)
             truck += price[i];
-        else    
-            bike+=price[i];
+        else
+            bike += price[i];
     }
-    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",car,truck,bike);
+
+    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f", car, truck, bike);
+
+    return 0;
 }
