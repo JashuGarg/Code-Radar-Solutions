3594b7a9eb2db72b42c5d906bcd8b4d2e67f4f84
Jashu Garg <jashugarg101@gmail.com>
2025-03-15 10:44:16 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -14,7 +14,7 @@ int main (){
     scanf("%f",&limit);
     for(int i=0;i<n;i++){
         if(i==0)
-            printf("Books above price &.2f:\n");
+            printf("Books above price %.2f:\n");
         
         if(price[i]>limit){
             printf("Title: %s, Author: %s, Price: %.2f",title[i],author[i],price[i]);
