764c80787d4daa37ece1a6f7c156fd937a599f67
Jashu Garg <jashugarg101@gmail.com>
2025-03-15 09:58:21 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -1,5 +1,6 @@
 // Your code here...
 #include <stdio.h>
+#include <limits.h>
 int main (){
     int n;
     scanf("%d",&n);
@@ -9,7 +10,7 @@ int main (){
     for(int i=0;i<n;i++){
         scanf("%d %s %f",&rollno[i],name[i],&marks[i]);
     }
-    int max = marks[0];
+    int max = INT_MIN;
     int index ;
     for (int i=0;i<n;i++){
         if (marks[i]>max)
