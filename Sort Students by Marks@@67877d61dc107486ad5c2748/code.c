933add355d45e9c0256fa524b1f1c40d6df739af
Jashu Garg <jashugarg101@gmail.com>
2025-03-16 23:07:18 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -1,21 +1,25 @@
-// Your code here...
 #include <stdio.h>
 #include <string.h>
 
-int main (){
+int main() {
     int n;
-    scanf("%d",&n);
-    int rollno[1000];
-    char name[n][100];
-    float marks[100];
-    for(int i=0;i<n;i++){
-        scanf("%d %s %f",&rollno[i],name[i],&marks[i]);
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    int roll[n];
+    char name[n][50];  
+    float marks[n];
+
+    // Input student data
+    printf("Enter roll number, name, and marks for each student:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d %s %f", &roll[i], name[i], &marks[i]);
     }
-    //sorting a marks array
-    for(int i=0;i<n;i++){
-        int isswapped = 0;
-        for (int j=0;j<n-i-1;j++){
-            if(marks[j+1]<marks[j]){
+
+    // Sorting based on marks in descending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (marks[i] < marks[j]) {
                 // Swap marks
                 float tempMarks = marks[i];
                 marks[i] = marks[j];
@@ -33,18 +37,11 @@ int main (){
                 strcpy(name[j], tempName);
             }
         }
-        if (isswapped=0)
-            break;
     }
 
-    //printing according to the marks
-
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            if (marks)
-        }
+    for (int i = 0; i < n; i++) {
+        printf("%d %s %.2f\n", roll[i], name[i], marks[i]);
     }
 
-
-
+    return 0;
 }
