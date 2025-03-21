3b1d7b4417843f18b2fd3bd754518f455ee1b135
Jashu Garg <jashugarg101@gmail.com>
2025-03-22 01:01:37 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
-// #include <stdlib.h>
-void swap (int *a,int *b){
-    int temp ;
-    temp =*a;
-    *a=*b;
-    *b=temp;
-}
+#include <stdlib.h>
 
 int findUnsortedSubarray(int arr[], int n) {
-    int count = 0;
-    for (int i=0;i<n;i++){
-        int index = arr[i];
-        for (int j=i+1;j<n;j++){
-            if (index<arr[j]){
-                index = arr[j];
-                swap(&index,&arr[j]);
-
-
-            }
-
-            else 
-                count++;
-            
-            
+    int start=-1,end=-1;
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            start = i;
+            break;
         }
-
     }
-    return count ;
+    
+    if (start==-1){
+        return 0;
+    }
+
+    if(arr[start]>arr[n-1])
+        end=n-1;
+    else{
+        for (int i = n - 1; i > 0; i--) {
+            if (arr[i] < arr[i - 1]) {
+                end = i;
+                break;
+            }
+        }
+    }
+
+    int min=arr[start],max=arr[start];
+    for (int i=start;i<=end;i++){
+        if (arr[i]<min)min=arr[i];
+        if (arr[i]>max)max=arr[i];
+    }
+
+    for (int i=0;i<start;i++){
+        if(arr[i]>min){
+            start=i;
+            break;
+        }
+    }
+
+    return (end - start + 1);
 }
