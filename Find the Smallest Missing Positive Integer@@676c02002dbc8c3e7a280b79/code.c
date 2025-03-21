773d2e4d414e1a8eeff53bdd198e5bf7a0595691
Jashu Garg <jashugarg101@gmail.com>
2025-03-22 01:27:35 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -2,21 +2,51 @@
 #include <stdio.h>
 #include <limits.h>
 
+void swap (int *a,int *b){
+    int temp ;
+    temp =*a;
+    *a=*b;
+    *b=temp;
+}
+
+void bubbleSort(int arr[],int n){
+    for (int i=n-2;i>=0;i--){
+        int isswapped = 0;
+        for (int j=0;j<=i;j++){
+            if (arr[j]>arr[j+1]){
+                swap(&arr[j],&arr[j+1]);
+                isswapped =1;
+            }
+        }
+        if (!isswapped)
+            break;
+        }
+}
+
+
 int main (){
     int n,arr[10000];
     scanf("%d",&n);
-    for (int i=0;i<n;i++){
+    for (int i=0;i<n;i++)
         scanf("%d",&arr[i]);
-    }
-    int sum=0;
-    for (int i=0;i<n;i++){
-        if (arr[i]<0)
-            arr[i]=0;
-        sum+=arr[i];
-    }
 
-    int sumn = (n*(n+1))/2 ;
-    printf("%d",sumn-sum);
+    bubbleSort(arr,n);
+
+    int start = 0, end = n-1,mid,ans;
+    while (start<=end)
+    {
+        mid = start + (end-start)/2;
+
+        if (arr[mid]-(mid+1)>0)
+            {
+                ans = mid;
+                end = mid-1;
+            }
+        else 
+            star = mid +1;
+    
+    }
+    printf("%d",ans+1);
 
 
 }
