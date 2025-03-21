1acf1d55ba41368dda741bd720234b23a3171760
Jashu Garg <jashugarg101@gmail.com>
2025-03-21 15:40:47 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
 #include <limits.h>
+int bubbleSort(int arr[],int n){
+    for (int i=n-2;i>=0;i--){
+        int isswapped = 0;
+        for (int j=0;j<i;j++){
+            if (arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                isswapped = 1;
+            }
+        }
+        if (!isswapped)
+            break;
+    }
+}
 int main (){
     int n,arr[100];
 
     scanf("%d",&n);
     for (int i=0;i<n;i++)
         scanf("%d",&arr[i]);
+    bubbleSort(arr,n);
+    int target = arr[n-1] - arr[0];
+    int start=0,end=n-1;
 
-    int max =  INT_MIN;
-    for (int i=0;i<n;i++){
-        if (arr[i]>max)
-            max=arr[i];
-    }
-    // printf("%d",max); 
-    int second = INT_MIN;
-
-    for (int i=0;i<n;i++){
-        if (arr[i]==max)
-            continue;
-        if (arr[i]>second){
-            second = arr[i];
+    while (start<end){
+        if (arr[end]-arr[start]<target)
+        {
+            target = arr[end]-arr[start];
+            start++;
+        }else {
+            end--;
         }
     }
 
-    printf("%d %d",second,max);
+    printf("%d",target);
+
+
 }
